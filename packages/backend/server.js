1f8cc4cebf3e5748177349ed9f892207aba2b0c1
pedroleite21 <pedrohleite@icloud.com>
2020-10-18 22:48:10 -0300

    Fix server

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,55 +1,74 @@
-require('dotenv').config({ path: '../../.env' });
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const Socket = require('socket.io');
+const path = require('path');
+
+let rootEnv;
+if (process.env.NODE_ENV === 'development') {
+  rootEnv = path.join(__dirname, '../', '../', '.env');
+} else {
+  rootEnv = path.join(__dirname, '../', '../', '../', '.env');
+}
+dotenv.config({ path: rootEnv });
 
 const db = require('./src/models');
 const initDb = require('./src/initDb');
 
-const app = express();
+const authRoutes = require('./src/routes/auth.routes');
+const categoryRoutes = require('./src/routes/category.routes');
+const customerRoutes = require('./src/routes/customer.routes');
+const itemRoutes = require('./src/routes/item.routes');
+const orderRoutes = require('./src/routes/order.routes');
+const uploadRoutes = require('./src/routes/upload.routes');
+const paymentRoutes = require('./src/routes/payment.routes');
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+async function startServer() {
+  const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  db.sequelize.sync({ force: true }).then(() => {
-    initDb.createAdminUser();
-    initDb.populatePaymentMethods();
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  if (process.env.NODE_ENV === 'development') {
+    db.sequelize.sync({ force: true }).then(() => {
+      initDb.createAdminUser();
+      initDb.populatePaymentMethods();
+    });
+  } else {
+    db.sequelize.sync();
+  }
+
+  app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to bezkoder application.' });
   });
-} else {
-  db.sequelize.sync();
+
+  app.use((_, res, next) => {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept',
+    );
+
+    next();
+  });
+
+  authRoutes(app);
+  categoryRoutes(app);
+  customerRoutes(app);
+  itemRoutes(app);
+  orderRoutes(app);
+  uploadRoutes(app);
+  paymentRoutes(app);
+
+  app.listen(3000, (error) => {
+    if (error) {
+      console.error('❌ Oops...');
+      console.error(error);
+      process.exit(1);
+    }
+
+    console.info('Server is running on port 3000.');
+  });
 }
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to bezkoder application.' });
-});
-
-app.use((_, res, next) => {
-  res.header(
-    'Access-Control-Allow-Headers',
-    'x-access-token, Origin, Content-Type, Accept',
-  );
-
-  next();
-});
-
-require('./src/routes/auth.routes')(app);
-require('./src/routes/category.routes')(app);
-require('./src/routes/customer.routes')(app);
-require('./src/routes/item.routes')(app);
-require('./src/routes/order.routes')(app);
-require('./src/routes/upload.routes')(app);
-require('./src/routes/payment.routes')(app);
-
-const server = app.listen(3000, () => {
-  console.log('Server is running on port 3000.');
-});
-
-/* Socket server */
-const io = new Socket(server);
-
-io.on('connection', () => {
-  console.log('Made socket connection');
-});
+startServer();
